a320bede5314ed67ac0922344183a00c676e67af
RatmirSemionov <144428344+RatmirSemionov@users.noreply.github.com>
2023-09-22 17:26:44 +0300

    Struktura

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+using std::vector;
+
+struct Studentas {
+string Vardas, Pavarde;
+vector <int> Pazymiai;
+int Egzaminas;
+float Vidurkis;
+};
