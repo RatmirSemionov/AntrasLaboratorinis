8be40c7f102c13389c51dfed159dbe23e681a98e
RatmirSemionov <144428344+RatmirSemionov@users.noreply.github.com>
2023-09-22 17:57:38 +0300

    Galutinio Vidurkio vaizdavimas

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using std::string;
 using std::cin;
 using std::endl;
 using std::vector;
+using std::setw;
 
 struct Studentas {
 string Vardas, Pavarde;
@@ -26,7 +27,7 @@ int main() {
   for (int i = 0; i < StudentuSkaicius; i++) {
     cout << "Iveskite studento varda ir pavarde: ";
     cin >> Laikinas.Vardas >> Laikinas.Pavarde;
-    cout "Iveskite pazymiu skaiciu: ";
+    cout << "Iveskite pazymiu skaiciu: ";
     cin >> PazymiuSkaicius;
     float PazymiuVidurkis = 0.0;
 
@@ -43,5 +44,13 @@ int main() {
     cin >> Laikinas.Egzaminas;
     //Galutinio Vidurkio skaiciavimas
     Laikinas.Vidurkis = 0.4*PazymiuVidurkis + 0.6*Laikinas.Egzaminas;
+    Grupe.push_back(Laikinas);
+  }
+  cout << "--------------------------------------------------" << endl;
+  cout << setw(10) << "Vardas " << setw(18) << " Pavarde " << setw(23) << " Galutinis (Vid.) " << endl;
+  cout << "--------------------------------------------------" << endl;
+  cout << std::fixed << std::setprecision(2);
+  for (auto &a: Grupe) {
+    cout << setw(10) << a.Vardas << setw(18) << a.Pavarde << setw(16) << a.Vidurkis << endl;
   }
 }
