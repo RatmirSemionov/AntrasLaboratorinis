4c76c7e2ceef2da96e2018fd5998e62dfeaf5481
RatmirSemionov <144428344+RatmirSemionov@users.noreply.github.com>
2023-09-27 12:35:40 +0300

    Pazymiu ivedimo budo pasirinkimas

--- main.cpp
+++ main.cpp
@@ -12,59 +12,83 @@ using std::vector;
 using std::setw;
 
 struct Studentas {
-string Vardas, Pavarde;
-vector <int> Pazymiai;
-int Egzaminas;
-float Vidurkis;
-double Mediana;
+    string Vardas, Pavarde;
+    vector<int> Pazymiai;
+    int Egzaminas;
+    float Vidurkis;
+    double Mediana;
 };
 
 int main() {
-  Studentas Laikinas;
-  vector <Studentas> Grupe;
-  int StudentuSkaicius, PazymiuSkaicius;
-  char Pasirinktas;
-  cout << "Iveskite studentu skaiciu: ";
-  cin >> StudentuSkaicius;
+    Studentas Laikinas;
+    vector<Studentas> Grupe;
+    int StudentuSkaicius, PazymiuSkaicius;
+    char Pasirinktas;
 
-  for (int i = 0; i < StudentuSkaicius; i++) {
-    cout << "Iveskite studento varda ir pavarde: ";
-    cin >> Laikinas.Vardas >> Laikinas.Pavarde;
-    cout << "Iveskite pazymiu skaiciu: ";
-    cin >> PazymiuSkaicius;
-    float PazymiuVidurkis = 0.0;
+    cout << "Iveskite studentu skaiciu: ";
+    cin >> StudentuSkaicius;
 
-    for (int j = 0; j < PazymiuSkaicius; j++) {
-      int k;
-      cout << "Iveskite " << j + 1 << " pazymi: ";
-      cin >> k;
-      Laikinas.Pazymiai.push_back(k);
-      PazymiuVidurkis += k;
+    for (int i = 0; i < StudentuSkaicius; i++) {
+        cout << "Iveskite studento varda ir pavarde: ";
+        cin >> Laikinas.Vardas >> Laikinas.Pavarde;
+
+        cout << "Iveskite pazymiu skaiciu (arba -1, jei norite ivesti pazymius be apribojimu): ";
+        cin >> PazymiuSkaicius;
+
+        if (PazymiuSkaicius == -1) {
+            cout << "Iveskite pazymius. Baigti ivedineti pazymius galite suvede '-1'." << endl;
+            int Numeris = 1;
+            while (true) {
+                int k;
+                cout << "Iveskite " << Numeris << " pazymi: ";
+                cin >> k;
+                if (k == -1) {
+                    break;
+                }
+                Laikinas.Pazymiai.push_back(k);
+                Numeris++;
+            }
+        } else {
+            for (int j = 0; j < PazymiuSkaicius; j++) {
+                int k;
+                cout << "Iveskite " << j + 1 << " pazymi: ";
+                cin >> k;
+                Laikinas.Pazymiai.push_back(k);
+            }
+        }
+
+        cout << "Iveskite egzamino pazymi: ";
+        cin >> Laikinas.Egzaminas;
+
+        // Galutinio Vidurkio skaiciavimas
+        float PazymiuVidurkis = 0.0;
+        for (int j = 0; j < Laikinas.Pazymiai.size(); j++) {
+            PazymiuVidurkis += Laikinas.Pazymiai[j];
+        }
+        if (!Laikinas.Pazymiai.empty()) {
+            PazymiuVidurkis /= Laikinas.Pazymiai.size();
+        }
+        Laikinas.Vidurkis = 0.4 * PazymiuVidurkis + 0.6 * Laikinas.Egzaminas;
+
+        // Medianos skaiciavimas
+        sort(Laikinas.Pazymiai.begin(), Laikinas.Pazymiai.end());
+        int vidut = Laikinas.Pazymiai.size() / 2;
+        if (Laikinas.Pazymiai.size() % 2 == 0) {
+            Laikinas.Mediana = (Laikinas.Pazymiai[vidut - 1] + Laikinas.Pazymiai[vidut]) / 2.0;
+        } else {
+            Laikinas.Mediana = Laikinas.Pazymiai[vidut];
+        }
+        Grupe.push_back(Laikinas);
+        Laikinas.Pazymiai.clear();
     }
-    PazymiuVidurkis /= PazymiuSkaicius;
 
-    cout << "Iveskite egzamino pazymi: ";
-    cin >> Laikinas.Egzaminas;
-    //Galutinio Vidurkio skaiciavimas
-    Laikinas.Vidurkis = 0.4*PazymiuVidurkis + 0.6*Laikinas.Egzaminas;
-    //Medianos skaiciavimas
-    sort(Laikinas.Pazymiai.begin(), Laikinas.Pazymiai.end());
-    int vidut = PazymiuSkaicius / 2;
-    if (PazymiuSkaicius % 2 == 0) Laikinas.Mediana = (Laikinas.Pazymiai[vidut - 1] +
-      Laikinas.Pazymiai[vidut]) / 2.0;
-    else Laikinas.Mediana = Laikinas.Pazymiai[vidut];
-    Grupe.push_back(Laikinas);
-    Laikinas.Pazymiai.clear();
-  }
-  cout << "Apskaiciuoti Vidurki (V) arba Mediana (M)? --> ";
-  cin >> Pasirinktas;
-  cout << "--------------------------------------------------" << endl;
-  cout << setw(10) << "Vardas " << setw(18) << " Pavarde " << setw(23) << (Pasirinktas == 'V' ? 
-    " Galutinis (Vid.) " : " Galutinis (Med.) ") << endl;
-  cout << "--------------------------------------------------" << endl;
-  cout << std::fixed << std::setprecision(2);
-  for (auto &a: Grupe) {
-    cout << setw(10) << a.Vardas << setw(18) << a.Pavarde << setw(16) << (Pasirinktas == 'V' ? 
-      a.Vidurkis : a.Mediana) << endl;
-  }
+    cout << "Apskaciuoti Vidurki (V) arba Mediana (M)? -> ";
+    cin >> Pasirinktas;
+    cout << "--------------------------------------------------" << endl;
+    cout << setw(10) << "Vardas " << setw(18) << " Pavarde " << setw(23) << (Pasirinktas == 'V' ? " Galutinis (Vid.) " : " Galutinis (Med.) ") << endl;
+    cout << "--------------------------------------------------" << endl;
+    cout << std::fixed << std::setprecision(2);
+    for (auto &a : Grupe) {
+        cout << setw(10) << a.Vardas << setw(18) << a.Pavarde << setw(16) << (Pasirinktas == 'V' ? a.Vidurkis : a.Mediana) << endl;
+    }
 }
