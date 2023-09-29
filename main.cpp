6077f5b410801d2602cb8dc4eafc8ac9ba4afa2d
RatmirSemionov <144428344+RatmirSemionov@users.noreply.github.com>
2023-09-29 10:49:26 +0300

    Failo skaitymo galimybe

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
+#include <sstream>
 
 using std::cout;
 using std::string;
@@ -12,6 +14,8 @@ using std::cin;
 using std::endl;
 using std::vector;
 using std::setw;
+using std::ifstream;
+using std::istringstream;
 
 struct Studentas {
     string Vardas, Pavarde;
@@ -27,90 +31,144 @@ int main() {
     int StudentuSkaicius, PazymiuSkaicius;
     char Pasirinktas;
     char Pasirinkimas;
+    char Pasirinkimas2;
     char PazymiuPasirinkimas;
+    string inputFileName;
 
-    cout << "Iveskite studentu skaiciu: ";
-    cin >> StudentuSkaicius;
+    cout << "Kaip norite ivesti duomenis? (M - manualiai, F - is failo): ";
+    cin >> Pasirinkimas2;
 
-    srand(time(0));
+    if (Pasirinkimas2 == 'F') {
+        cout << "Iveskite failo pavadinima: ";
+        cin >> inputFileName;
+        ifstream inputFile(inputFileName);
 
-    for (int i = 0; i < StudentuSkaicius; i++) {
-        cout << "Iveskite studento varda ir pavarde: ";
-        cin >> Laikinas.Vardas >> Laikinas.Pavarde;
-        //Pasirinkimas kaip gausim duomenis apie pazymius(Ivedimo metodu arba generuojamu)
-        cout << "Pasirinkite kaip ivesite pazymius (M - manualiai, A - automatiskai): ";
-        cin >> Pasirinkimas;
+        if (!inputFile.is_open()) {
+            cout << "Nepavyko atidaryti faila: " << inputFileName << endl;
+            return 1;
+        }
 
-        if (Pasirinkimas == 'A') {
-            cout << "Ar zinote, kiek pazymiu reikia sugeneruot? (T - taip, N - ne) ";
-            cin >> PazymiuPasirinkimas;
-            if (PazymiuPasirinkimas == 'T') {
-            cout << "Iveskite generuojamu pazymiu skaiciu: ";
-            cin >> PazymiuSkaicius;
+        string line;
+        getline(inputFile, line); // Perskaityt header linija ir pasalint ji
+        while (getline(inputFile, line)) {
+            istringstream iss(line);
+            iss >> Laikinas.Vardas >> Laikinas.Pavarde;
+
+            int pazymys;
+            while (iss >> pazymys) {
+                Laikinas.Pazymiai.push_back(pazymys);
             }
-            else if (PazymiuPasirinkimas == 'N') {
-                PazymiuSkaicius = rand() % 11;
-            }
+            Laikinas.Egzaminas = Laikinas.Pazymiai.back();
+            Laikinas.Pazymiai.pop_back();
 
-            for (int j = 0; j < PazymiuSkaicius; j++) {
-                int RandPazymis = rand() % 11;
-                Laikinas.Pazymiai.push_back(RandPazymis);
+            // Skaiciavimas
+            float PazymiuVidurkis = 0.0;
+            for (int j = 0; j < Laikinas.Pazymiai.size(); j++) {
+                PazymiuVidurkis += Laikinas.Pazymiai[j];
             }
-        } else if (Pasirinkimas == 'M') {
-            cout << "Iveskite pazymiu skaiciu (arba -1, jei norite ivesti pazymius be apribojimu): ";
-            cin >> PazymiuSkaicius;
-
-            if (PazymiuSkaicius == -1) {
-                cout << "Iveskite pazymius. Baigti ivedineti pazymius galite suvede '-1'." << endl;
-                int Numeris = 1;
-                while (true) {
-                    int k;
-                    cout << "Iveskite " << Numeris << " pazymi: ";
-                    cin >> k;
-                    if (k == -1) {
-                        break;
-                    }
-                    Laikinas.Pazymiai.push_back(k);
-                    Numeris++;
-                }
+            if (!Laikinas.Pazymiai.empty()) {
+                PazymiuVidurkis /= Laikinas.Pazymiai.size();
+            }
+            //Galutinio Vidurkio skaiciavimas
+            Laikinas.Vidurkis = 0.4 * PazymiuVidurkis + 0.6 * Laikinas.Egzaminas;
+            //Medianos skaiciavimas
+            sort(Laikinas.Pazymiai.begin(), Laikinas.Pazymiai.end());
+            int vidut = Laikinas.Pazymiai.size() / 2;
+            if (Laikinas.Pazymiai.size() % 2 == 0) {
+                Laikinas.Mediana = (Laikinas.Pazymiai[vidut - 1] + Laikinas.Pazymiai[vidut]) / 2.0;
             } else {
+                Laikinas.Mediana = Laikinas.Pazymiai[vidut];
+            }
+
+
+            Grupe.push_back(Laikinas);
+            Laikinas.Pazymiai.clear();
+        }
+        inputFile.close();
+    } else if (Pasirinkimas2 == 'M') {
+        cout << "Iveskite studentu skaiciu: ";
+        cin >> StudentuSkaicius;
+
+        srand(time(0));
+
+        for (int i = 0; i < StudentuSkaicius; i++) {
+            cout << "Iveskite studento varda ir pavarde: ";
+            cin >> Laikinas.Vardas >> Laikinas.Pavarde;
+            //Pasirinkimas kaip gausim duomenis apie pazymius(Ivedimo metodu arba generuojamu)
+            cout << "Pasirinkite kaip ivesite pazymius (M - manualiai, A - automatiskai): ";
+            cin >> Pasirinkimas;
+
+            if (Pasirinkimas == 'A') {
+                    cout << "Ar zinote, kiek pazymiu reikia sugeneruot? (T - taip, N - ne) ";
+                    cin >> PazymiuPasirinkimas;
+                    if (PazymiuPasirinkimas == 'T') {
+                    cout << "Iveskite generuojamu pazymiu skaiciu: ";
+                    cin >> PazymiuSkaicius;
+                    }
+                    else if (PazymiuPasirinkimas == 'N') {
+                        PazymiuSkaicius = rand() % 11;
+                    }
+
                 for (int j = 0; j < PazymiuSkaicius; j++) {
-                    int k;
-                    cout << "Iveskite " << j + 1 << " pazymi: ";
-                    cin >> k;
-                    Laikinas.Pazymiai.push_back(k);
+                    int RandPazymis = rand() % 11;
+                    Laikinas.Pazymiai.push_back(RandPazymis);
+                }
+            } else if (Pasirinkimas == 'M') {
+                cout << "Iveskite pazymiu skaiciu (arba -1, jei norite ivesti pazymius be apribojimu): ";
+                cin >> PazymiuSkaicius;
+
+                if (PazymiuSkaicius == -1) {
+                    cout << "Iveskite pazymius. Baigti ivedineti pazymius galite suvede '-1'." << endl;
+                    int Numeris = 1;
+                    while (true) {
+                        int k;
+                        cout << "Iveskite " << Numeris << " pazymi: ";
+                        cin >> k;
+                        if (k == -1) {
+                            break;
+                        }
+                        Laikinas.Pazymiai.push_back(k);
+                        Numeris++;
+                    }
+                } else {
+                    for (int j = 0; j < PazymiuSkaicius; j++) {
+                        int k;
+                        cout << "Iveskite " << j + 1 << " pazymi: ";
+                        cin >> k;
+                        Laikinas.Pazymiai.push_back(k);
+                    }
                 }
             }
-        }
-        //Pasirinkimas kaip gausim egzamino pazymi(Ivedimo metodu arba generuojamu)
-        cout << "Pasirinkite kaip ivesite egzamino pazymi (M - manualiai, A - automatiskai): ";
-        cin >> Pasirinkimas;
+            //Pasirinkimas kaip gausim egzamino pazymi(Ivedimo metodu arba generuojamu)
+            cout << "Pasirinkite kaip ivesite egzamino pazymi (M - manualiai, A - automatiskai): ";
+            cin >> Pasirinkimas;
 
-        if (Pasirinkimas == 'A') {
-            Laikinas.Egzaminas = rand() % 11;
-        } else if (Pasirinkimas == 'M') {
-            cout << "Iveskite egzamino pazymi: ";
-            cin >> Laikinas.Egzaminas;
+            if (Pasirinkimas == 'A') {
+                Laikinas.Egzaminas = rand() % 11;
+            } else if (Pasirinkimas == 'M') {
+                cout << "Iveskite egzamino pazymi: ";
+                cin >> Laikinas.Egzaminas;
+            }
+            float PazymiuVidurkis = 0.0;
+            for (int j = 0; j < Laikinas.Pazymiai.size(); j++) {
+                PazymiuVidurkis += Laikinas.Pazymiai[j];
+            }
+            if (!Laikinas.Pazymiai.empty()) {
+                PazymiuVidurkis /= Laikinas.Pazymiai.size();
+            }
+            //Galutinio Vidurkio skaiciavimas
+            Laikinas.Vidurkis = 0.4 * PazymiuVidurkis + 0.6 * Laikinas.Egzaminas;
+            //Medianos skaiciavimas
+            sort(Laikinas.Pazymiai.begin(), Laikinas.Pazymiai.end());
+            int vidut = Laikinas.Pazymiai.size() / 2;
+            if (Laikinas.Pazymiai.size() % 2 == 0) {
+                Laikinas.Mediana = (Laikinas.Pazymiai[vidut - 1] + Laikinas.Pazymiai[vidut]) / 2.0;
+            } else {
+                Laikinas.Mediana = Laikinas.Pazymiai[vidut];
+            }
+            Grupe.push_back(Laikinas);
+            Laikinas.Pazymiai.clear();
         }
-        float PazymiuVidurkis = 0.0;
-        for (int j = 0; j < Laikinas.Pazymiai.size(); j++) {
-            PazymiuVidurkis += Laikinas.Pazymiai[j];
-        }
-        if (!Laikinas.Pazymiai.empty()) {
-            PazymiuVidurkis /= Laikinas.Pazymiai.size();
-        }
-        //Galutinio Vidurkio skaiciavimas
-        Laikinas.Vidurkis = 0.4 * PazymiuVidurkis + 0.6 * Laikinas.Egzaminas;
-        //Medianos skaiciavimas
-        sort(Laikinas.Pazymiai.begin(), Laikinas.Pazymiai.end());
-        int vidut = Laikinas.Pazymiai.size() / 2;
-        if (Laikinas.Pazymiai.size() % 2 == 0) {
-            Laikinas.Mediana = (Laikinas.Pazymiai[vidut - 1] + Laikinas.Pazymiai[vidut]) / 2.0;
-        } else {
-            Laikinas.Mediana = Laikinas.Pazymiai[vidut];
-        }
-        Grupe.push_back(Laikinas);
-        Laikinas.Pazymiai.clear();
     }
 
     cout << "Apskaciuoti Vidurki (V) arba Mediana (M)? -> ";
