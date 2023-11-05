a2411e2402d9e5f77de7293f2a3e7899b1829966
RatmirSemionov <144428344+RatmirSemionov@users.noreply.github.com>
2023-11-05 21:17:32 +0200

    Funkciju pakeitimas

--- mylib.h
+++ mylib.h
@@ -47,6 +47,6 @@ void PazymiuGeneravimas(Studentas& Laikinas);
 void PazymiuIvedimas(Studentas& Laikinas);
 void StudentuInfo(Studentas& Laikinas, vector<Studentas>& Grupe);
 void SukurtiStudentoFaila(int studentCount, int gradeCount, const string& filename);
-void KategorizuotiStudentus(const vector<Studentas>& Grupe, vector<Studentas>& BelowFive, vector<Studentas>& AboveFive);
-void KategorijuFailai(const vector<Studentas>& BelowFive, const vector<Studentas>& AboveFive);
+void KategorizuotiStudentus(vector<Studentas>& Grupe, vector<Studentas>& BelowFive, vector<Studentas>& AboveFive);
+void KategorijuFailai(const vector<Studentas>& Grupe, const vector<Studentas>& BelowFive, const vector<Studentas>& AboveFive);
 #endif // MYLIB_H_INCLUDED
